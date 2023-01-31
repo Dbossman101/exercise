56e3cd08f75e8ebbf973079c0e528e61686cb1cd
Dbossman101 <daveadetoks@gmail.com>
2023-01-31 13:52:50 +0100

    Initial Commit

--- src/com/assignment/SavingsAccount.java
+++ src/com/assignment/SavingsAccount.java
@@ -1,3 +1,12 @@
+//Create   a  SavingsAccount   class.
+//Use   a   static   data  member  annualInterestRate to store  the annual interest rate for each of the savers.
+//Each member of the class contains a private data member savingsBalance indicating the amount the saver currently has ondeposit.
+//Provide member function calculateMonthlyInterest that calculates the monthlyinterest by multiplying the  balance  by  annualInterestRate  divided  by  12;  this  interestshould be added to savingsBalance.
+// Provide a static member functionmodifyInterestRate that sets the static annualInterestRate to a new value.
+//Write a driverprogram   to   test   class   SavingsAccount.
+//Instantiate   two   different   objects   of   classSavingsAccount,   saver1   and   saver2,   with   balances   of   $2000.00   and   $3000.00,respectively.
+//Set   the   annualInterestRate   to   3   percent.   Then   calculate   the   monthlyinterest   and   print   the   new   balances   for   each   of   the   savers.Then set theannualInterestRate to 4 percent,
+//calculate the next month's interest and print the newbalances for each of the savers.
 package com.assignment;
 import java.util.Scanner;
 public class SavingsAccount {
